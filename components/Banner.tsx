1abcdac3e17a47e289ca978b10ab819cb61c3fbe
deoworld <doriazowan@gmail.com>
2023-10-21 00:00:14 +0100

    edited Undergoing to under

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,7 @@ const OverlayBanner = () => {
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black  bg-opacity-70 text-textGreen text-center flex justify-center items-center z-50">
       <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold border border-textGreen rounded-lg p-6 bg-greenOverlay">
-        🚧🛠️ Website Undergoing Reconstruction 👷🏗️
+        🚧🛠️ Website Under Reconstruction 👷🏗️
       </h1>
     </div>
   );
