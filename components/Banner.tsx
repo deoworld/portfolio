6edccf1e1e76f118f1ff67af73fd523ca5977540
deoworld <doriazowan@gmail.com>
2023-10-30 13:23:11 +0100

    Commented out OverlayBanner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,14 +1,14 @@
 import { motion } from "framer-motion";
 
-const OverlayBanner = () => {
-  return (
-    <div className="fixed top-0 left-0 w-full h-full bg-black  bg-opacity-70 text-textGreen text-center flex justify-center items-center z-50">
-      <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold border border-textGreen rounded-lg p-6 bg-greenOverlay">
-        🚧🛠️ Website Under Reconstruction 👷🏗️
-      </h1>
-    </div>
-  );
-};
+// const OverlayBanner = () => {
+//   return (
+//     <div className="fixed top-0 left-0 w-full h-full bg-black  bg-opacity-70 text-textGreen text-center flex justify-center items-center z-50">
+//       <h1 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold border border-textGreen rounded-lg p-6 bg-greenOverlay">
+//         🚧🛠️ Website Under Reconstruction 👷🏗️
+//       </h1>
+//     </div>
+//   );
+// };
 
 const Banner = () => {
   return (
